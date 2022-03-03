0e8dac9550a741f68aeb564dc56f2314f28bdefa
lim950808 <lim080895@gmail.com>
2022-03-04 00:11:19 +0900

    ch03

---
+++ ch03/src/ch02/BitOperationExam01.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class BitOperationExam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte flag = 0b00001010; //각 비트는 8개의 센서 비트
+		
+		if((flag & 0b00001000) == 0) //비트 논리 연산 결과가 0인지 비교
+			System.out.println("온도는 0도 이하");
+		else
+			System.out.println("온도는 0도 이상");
+
+	}
+
+}
---
+++ ch03/src/ch02/BitwiserOperationExam1.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class BitwiserOperationExam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean play = true;
+		play = !play; //논리부정
+		
+		System.out.println(play);
+		
+		//비트와이즈 연산에만 사용
+		//play = ~play;
+
+	}
+
+}
