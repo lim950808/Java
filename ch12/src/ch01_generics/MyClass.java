813100d4635975d15657a9bcdd857347c3e8d7d8
lim950808 <lim080895@gmail.com>
2022-03-14 00:01:23 +0900

    ch12

---
+++ ch12/src/ch01_generics/MyClass.java
@@ -0,0 +1,13 @@
+package ch01_generics;
+
+public class MyClass<T> {
+	T val;
+	void set(T a) {
+		val=a;
+	}
+	
+	T get() {
+		return val;
+	}
+
+}
---
+++ ch12/src/ch01_generics/MyClassMain.java
@@ -0,0 +1,17 @@
+package ch01_generics;
+
+public class MyClassMain {
+public static void main(String[] args) {
+	//컴파일시 T -> Integer로 변환
+	MyClass<Integer> s = new MyClass<Integer>();
+	s.set(10); //10 -> Integer10으로 autoBoxing되어 들어감
+	s.set(new Integer(10));
+	
+	//컴파일시 T -> String으로 변환
+	MyClass<String> s1 = new MyClass<String>();
+	s1.set("hello");
+	System.out.println(s1.get());
+	
+	
+}
+}
